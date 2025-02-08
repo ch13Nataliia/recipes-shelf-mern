cdc9958d7fbc90373312a9847f4c0866eeab2ceb
Nataliia Tsirul <tcirulnatalya@gmail.com>
2025-02-08 13:27:04 +0000

    updated product routes

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import Product from '../models/product.model.js';
+import mongoose from 'mongoose';
 
 // FIND ALL PRODUCTS
 export const getProducts = async (req, res) => {
--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -2,6 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import Product from '../models/product.model.js';
 import { getProducts } from '../controllers/product.controller.js';
+import { getSingleProduct } from '../controllers/product.controller.js';
+import { createProduct } from '../controllers/product.controller.js';
+import { updateProduct } from '../controllers/product.controller.js';
+import { deleteProduct } from '../controllers/product.controller.js';
+
 
 const router = express.Router();
 
