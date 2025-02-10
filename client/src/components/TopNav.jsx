72a495fc0c4c6fd8dfcaa3615acfd2ed6d980bb1
Nataliia Tsirul <tcirulnatalya@gmail.com>
2025-02-10 11:19:37 +0000

    created navbar

---
+++ client/src/components/TopNav.jsx
@@ -0,0 +1,55 @@
+// import React from 'react'
+import {
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Link,
+  Text,
+  useColorMode,
+} from '@chakra-ui/react';
+import { FaPlusCircle } from 'react-icons/fa';
+
+import { FiSun } from 'react-icons/fi';
+import { FaMoon } from 'react-icons/fa';
+
+const TopNav = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Container maxW={'1140px'} px={4}>
+      <Flex
+        h={16}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        flexDir={{
+          base: 'column',
+          sm: 'row',
+        }}
+      >
+        <Text
+          fontSize={{ base: '22', sn: '28' }}
+          fontWeight={'bold'}
+          textTransform={'uppercase'}
+          textAlign={'center'}
+          bgGradient="linear(to-r,rgb(181, 137, 224),rgb(225, 85, 155))"
+          bgClip="text"
+        >
+          <Link to={'/'}>Dessert Recipes</Link>
+        </Text>
+
+        <HStack spacing={2} alignItems={'center'}>
+          <Link to={'/create'}>
+            <Button>
+              <FaPlusCircle fontSize={20} />
+            </Button>
+          </Link>{' '}
+          <Button onClick={toggleColorMode}>
+            {colorMode === 'light' ? <FaMoon /> : <FiSun size="20" />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default TopNav;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+      <div>HomePage</div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat
+        fugiat consequatur officia, esse asperiores, adipisci commodi est error,
+        cum culpa aliquam? Tempora earum dolorem, enim eaque doloremque ea,
+        labore unde sint consequuntur nostrum assumenda modi eveniet repellat
+        quidem sit cum magni? Dolor animi sit pariatur fugiat explicabo,
+        aspernatur quia harum!
+      </p>
+    </>
+  );
+};
+
+export default HomePage;
